affc686788aa13db9b7c9dcb2af92ea39a6728de
Ahmed Younis <ayounis@innotech.com.eg>
2023-02-05 23:07:36 +0200

    Add project files.

---
+++ SmartFarm/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartFarm;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SmartFarm/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartFarm;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
